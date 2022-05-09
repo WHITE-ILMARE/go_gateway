dc1d733c3cd472430a598392fa61165a56511fd3
WHITE-ILMARE <2480800244@qq.com>
2022-05-09 15:04:41 +0800

    doing load_balance demo test

--- gateway_demos/proxy/reverse_proxy/main.go
+++ gateway_demos/proxy/reverse_proxy/main.go
@@ -2,7 +2,12 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+	"net/url"
 )
+
+var addr = "127.0.0.1:2002"
 
 func main() {
 	// 基础知识补充
@@ -12,4 +17,19 @@ func main() {
 	// len()参数为字符串时返回的也是字节数而不是字符数
 	fmt.Printf("len('中')=%d\n", len("中"))
 	// 基础知识补充结束
+
+	rs1 := "http://127.0.0.1:2003/base"
+	url1, err1 := url.Parse(rs1)
+	if err1 != nil {
+		log.Println(err1)
+	}
+	rs2 := "http://127.0.0.1:2004/base"
+	url2, err2 := url.Parse(rs2)
+	if err2 != nil {
+		log.Println(err2)
+	}
+	urls := []*url.URL{url1, url2}
+	proxy := NewMultipleHostsReverseProxy(urls)
+	log.Println("Starting http server at " + addr)
+	log.Fatal(http.ListenAndServe(addr, proxy))
 }
